9dbd16a31911277eb0c0e7b41ef2f4d36bea8993
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-02-04 19:08:40 -0800

    Add Feb 4 blog post, archiving January.

--- js/sitelets.js
+++ js/sitelets.js
@@ -8,7 +8,12 @@ var blogLi = addElement(outerUL,"","","li")
 addElement(blogLi,"Blog ▼","","a")
 var blogUL = addElement(blogLi,"","","ul")
 
-addElement(addElement(blogUL,"","","li"),"Jan 2023","","a","","/blog.html")
+addElement(addElement(blogUL,"","","li"),"Feb 2023","","a","","/blog.html")
+
+var $2023Li = addElement(blogUL,"","","li")
+addElement($2023Li,"2023 ▼","","a")
+var $2023UL = addElement($2023Li,"","","ul")
+addElement(addElement($2023UL,"","","li"),"Jan 2023","","a","","/2023/January.html")
 
 var $2022Li = addElement(blogUL,"","","li")
 addElement($2022Li,"2022 ▼","","a")
