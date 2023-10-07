8a1313caae8d44bafc2bfd70ebc0a024fbbb005e
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-10-07 11:18:42 -0800

    Add Oct 7 blog post. Also add to contact page.

--- js/sitelets.js
+++ js/sitelets.js
@@ -9,11 +9,12 @@ var blogLi = addElement(outerUL,"","","li")
 addElement(blogLi,"Blog ▼","","a")
 var blogUL = addElement(blogLi,"","","ul")
 
-addElement(addElement(blogUL,"","","li"),"Sept 2023","","a","","/blog.html")
+addElement(addElement(blogUL,"","","li"),"Oct 2023","","a","","/blog.html")
 
 var $2023Li = addElement(blogUL,"","","li")
 addElement($2023Li,"2023 ▼","","a")
 var $2023UL = addElement($2023Li,"","","ul")
+addElement(addElement(blogUL,"","","li"),"Sept 2023","","a","","/2023/September.html")
 addElement(addElement($2023UL,"","","li"),"Aug 2023","","a","","/2023/August.html")
 addElement(addElement($2023UL,"","","li"),"July 2023","","a","","/2023/July.html")
 addElement(addElement($2023UL,"","","li"),"June 2023","","a","","/2023/June.html")
